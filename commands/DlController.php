5bad35b7cdad587b93e1f378fd8e83d9b1740c8c
brian3t <ngxtri@gmail.com>
2018-07-06 14:51:06 -0700

    Working on scraping https://www.sandiegoreader.com/events/search/?category=Genre&start_date=2018-07-04&end_date=2018-07-04 Pulling venue name. Todo: saving url into system_note

--- commands/DlController.php
+++ commands/DlController.php
@@ -6,6 +6,7 @@ use app\models\Event;
 use app\models\Venue;
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
+use Yii;
 use yii\console\Controller;
 use yii\db\Exception;
 
@@ -20,6 +21,7 @@ define('SDREADER_LOCAL', 'http://lnoapi/tmp/Eventsearch_SanDiegoReader.html');
 class DlController extends Controller
 {
     public $SCRAPER_ID = null;
+
     public function init()
     {
         $this->SCRAPER_ID = \Yii::$app->params['scraper_id'];
@@ -36,16 +38,44 @@ class DlController extends Controller
         $client = new Client();
 //        $crawler = $client->request('GET', SDREADER, ['start_date' => '2018-07-04', 'end_date' => '2018-07-04']);
         $crawler = $client->request('GET', SDREADER_LOCAL, ['start_date' => '2018-07-04', 'end_date' => '2018-07-04']);
-        $crawler->filter('table.event_list tr')->each(function ($node){
-            /** @var Crawler $node */
+        $crawler->filter('table.event_list tr')->each(function ($event_and_venue) use (&$records){
+            /** @var Crawler $event_and_venue */
+            [$venue_name, $venue_href] = current($event_and_venue->filter('h5.place > a')->extract(['_text', 'href']));
             //find out if venue already exists
-            $venue_name = $node->extract('h5.place');
             $venue_exist = Venue::findOne(['name' => $venue_name]);
-            $venue = new Venue();
-            $venue->setAttributes(['created_by' => $this->SCRAPER_ID]);
-
+            if (!$venue_exist instanceof Venue) {
+                $venue = new Venue();
+                $venue->setAttributes(['name' => $venue_name, 'system_note' => $venue_href]);
+                $venue->save();
+                $venue_id = $venue->id;
+            } else {
+                $venue_id = $venue_exist->id;
+            }
+            if (!is_int($venue_id)) {
+                return;
+            }
+            Yii::$app->db->createCommand("UPDATE venue SET `created_by` = :created_by WHERE `id` = :venue_id")->bindValues([':created_by' => $this->SCRAPER_ID, ':venue_id' => $venue_id])->execute();
+            $records++;
+            [$event_name, $event_href] = current($event_and_venue->filter('h4 > a')->extract(['_text', 'href']));
+            //find out if event already exists
             $event = new Event();
-            $event->setAttributes(['created_by' => $this->SCRAPER_ID]);
+            $event_exist = Event::findOne(['name' => $event_name]);
+            if (!$event_exist instanceof Event) {
+                $event = new Event();
+                $event->setAttributes(['created_by' => $this->SCRAPER_ID, 'name' => $event_name, 'system_note' => $event_href]);
+                $time = $event_and_venue->filter('td.time')->text();
+                $city = $event_and_venue->filter('td.city>ul>li>a')->text();
+                $description = implode(', ', $event_and_venue->filter('td.category > ul li')->extract(['_text']));
+                $event->venue_id = $venue_id;
+                $event->date = '2018-07-04';
+                $event->setAttributes(compact(['time', 'city', 'description']));
+                $event->save();
+                $event_id = $event->id;
+                if (is_int($event_id)) {
+                    Yii::$app->db->createCommand("UPDATE event SET `created_by` = :created_by WHERE `id` = :id")->bindValues([':created_by' => $this->SCRAPER_ID, ':id' => $event_id])->execute();
+                }
+                $records++;
+            }
         });
         echo "Pulled this much: " . $records . " records." . PHP_EOL;
         return 1;
--- models/Venue.php
+++ models/Venue.php
@@ -9,24 +9,6 @@ use app\models\base\Venue as BaseVenue;
  */
 class Venue extends BaseVenue
 {
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return array_replace_recursive(parent::rules(),
-	    [
-            [['created_at', 'updated_at'], 'safe'],
-            [['created_by', 'user_id'], 'integer'],
-            [['lat', 'lng', 'cost'], 'number'],
-            [['name', 'address1', 'address2', 'website', 'twitter', 'facebook'], 'string', 'max' => 255],
-            [['city'], 'string', 'max' => 80],
-            [['state'], 'string', 'max' => 8],
-            [['zip'], 'string', 'max' => 25],
-            [['description'], 'string', 'max' => 800],
-            [['phone'], 'string', 'max' => 18]
-        ]);
-    }
 
     public function pull_address(){
         $full = implode(', ', [$this->address1, $this->address2, $this->city, $this->state, $this->zip]);
