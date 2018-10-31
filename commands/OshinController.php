eaa48ff212b82ed61af147fbdd676dd3c990042b
brian3t <ngxtri@gmail.com>
2018-10-31 14:06:21 -0700

    oshin rate band

--- commands/OshinController.php
+++ commands/OshinController.php
@@ -23,8 +23,21 @@ class OshinController extends Controller
         $db = \Yii::$app->db;
         $db->createCommand("UPDATE event SET temp = CAST(REGEXP_REPLACE(system_note, '(?<!https:)//', '/') AS CHAR) WHERE source='sdr'")->execute();
         $db->createCommand("UPDATE event SET system_note = temp WHERE temp IS NOT NULL AND source='sdr'")->execute();
-        $db->createCommand("update event set created_by = 35 where created_by is null;")->execute();
+        $db->createCommand("UPDATE event SET created_by = 35 WHERE created_by IS NULL;")->execute();
         echo "Cleanup done" . PHP_EOL;
+        return 1;
+    }
+
+    /**
+     * Clean up data
+     * Clean up sdr systemnote url
+     * @throws Exception
+     */
+    public function actionRandomRateBand()
+    {
+        $db = \Yii::$app->db;
+        $db->createCommand("UPDATE band SET lno_score = ROUND((RAND() * (10-5)) + 5 )")->execute();
+        echo "Random rate done" . PHP_EOL;
         return 1;
     }
 
