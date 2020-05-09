aa896236f8d2013c2b6c13abc95ee5634a9a9b46
brian3t <ngxtri@gmail.com>
2020-05-08 21:53:23 -0700

    kris admin

--- config/web.php
+++ config/web.php
@@ -106,7 +106,7 @@ $config = [
         'user' => [
             'class' => Da\User\Module::class,
             // ...other configs from here: [Configuration Options](installation/configuration-options.md), e.g.
-             'administrators' => ['admin','ngxtri'], // this is required for accessing administrative actions
+             'administrators' => ['admin','ngxtri','windlinx@hotmail.com'], // this is required for accessing administrative actions
             // 'generatePasswords' => true,
             // 'switchIdentitySessionKey' => 'myown_usuario_admin_user_key',
         ],
