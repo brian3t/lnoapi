f9b3db8ae2821247494ea02c19f723d9159bf132
brian3t <ngxtri@gmail.com>
2021-03-14 08:34:04 -0700

    Minor fixes. WIP: upgrade Yii2.

--- views/event/_search.php
+++ views/event/_search.php
@@ -18,14 +18,6 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'id', ['template' => '{input}'])->textInput(['style' => 'display:none']); ?>
     <?= $form->field($model, 'name')->textInput(['maxlength' => true, 'placeholder' => 'Name']) ?>
 
-<!--    --><?/*= $form->field($model, 'user_id')->widget(\kartik\widgets\Select2::classname(), [
-        'data' => \yii\helpers\ArrayHelper::map(\app\models\User::find()->orderBy('id')->asArray()->all(), 'id', 'username'),
-        'options' => ['placeholder' => 'Choose User'],
-        'pluginOptions' => [
-            'allowClear' => true
-        ],
-    ]); */?>
-
     <?= $form->field($model, 'source')->widget(\kartik\widgets\Select2::classname(), [
         'data' => ['sdr'=>'San Diego Reader (sdr)','ticketfly'=>'TicketFly','reverb'=>'Reverb','tickmas'=>'Ticket Master (tickmas)','other'=>'Other','unknown'=>'Unknown'],
         'options' => ['placeholder' => 'Choose Source'],
