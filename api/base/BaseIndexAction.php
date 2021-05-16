b59efeb140f3841f3072fc569afd9a3d10986da5
brian3t <ngxtri@gmail.com>
2021-05-15 21:41:00 -0700

    API now accept query_array

--- api/base/BaseIndexAction.php
+++ api/base/BaseIndexAction.php
@@ -9,10 +9,15 @@ class BaseIndexAction extends IndexAction
 {
     /**
      * Prepares the data provider that should return the requested collection of the models.
+     * Acceptable params:
+     * ?qa=query_array, JSON string of queries in array format, such as
+     * [
+     *  ['not', ['column' => value]]
+     * ]
+     * Read Yii2 where condition for format
      * @return ActiveDataProvider
      */
-    protected function prepareDataProvider()
-    {
+    protected function prepareDataProvider() {
         $modelClass = new $this->modelClass();
         if ($this->prepareDataProvider !== null) {
             return call_user_func($this->prepareDataProvider, $this);
@@ -21,13 +26,27 @@ class BaseIndexAction extends IndexAction
         /* @var $modelClass \yii\db\BaseActiveRecord */
         $params = \Yii::$app->request->queryParams;
         unset($params['expand']);
+        unset($params['_']);
         unset($params['page']);
-        $page_size = $params['page_size']??false;
+        $page_size = $params['page_size'] ?? false;
         unset($params['page_size']);
+        $qr = $params['qr'] ?? false;//query raw
+        unset($params['qr']);
+        try {
+            $qr_array = json_decode($qr, JSON_OBJECT_AS_ARRAY);
+        } catch (\Exception $e) {
+            $qr_array = false;
+        }
+        $dp_query = $modelClass::find()->where($params);
+        if ($qr_array) {
+            foreach ($qr_array as $qr_cond) {
+                $dp_query = $dp_query->andWhere($qr_cond);//e.g. ['not', ['column' => value]]
+            }
+        }
         $ap = new ActiveDataProvider([
-            'query' => $modelClass::find()->where($params),
+            'query' => $dp_query,
         ]);
-        if($page_size) {
+        if ($page_size) {
             $ap->pagination->setPageSize($page_size);
         }
         return $ap;
